bda9a9b3b3e6b25b764e018b0b601b1dc0ad3bcc
cdanh <danhcong6205@gmail.com>
2026-03-19 03:11:05 +0700

    feat: thêm hằng số phân quyền, phương thức xét tuyển vào Constants

--- src/main/java/com/tuyensinh/util/Constants.java
+++ src/main/java/com/tuyensinh/util/Constants.java
@@ -1,5 +1,25 @@
 package com.tuyensinh.util;
 
 public class Constants {
-    
+
+    // Thông tin app
+    public static final String APP_TITLE   = "Phần Mềm Quản Lý Tuyển Sinh - Đại học Sài Gòn";
+    public static final String APP_VERSION = "1.0.0";
+
+    // Phân trang
+    public static final int PAGE_SIZE = 20;
+
+    // Phương thức xét tuyển
+    public static final String PT_THPT = "THPT";
+    public static final String PT_VSAT = "VSAT";
+    public static final String PT_DGNL = "DGNL";
+
+    // Kết quả xét tuyển
+    public static final String KQ_CHO         = "CHO";
+    public static final String KQ_TRUNG_TUYEN = "TRUNG_TUYEN";
+    public static final String KQ_KHONG_DAT   = "KHONG_DAT";
+
+    // Phân quyền
+    public static final String ROLE_ADMIN = "ADMIN";
+    public static final String ROLE_USER  = "USER";
 }
