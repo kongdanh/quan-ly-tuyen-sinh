08a2a3ae7ae4bb4d7e06b605da5e38591bfc0469
cdanh <danhcong6205@gmail.com>
2026-03-20 02:58:05 +0700

    feat(dao): add DAO for permission and diem thi xet tuyen

---
+++ src/main/java/com/tuyensinh/dao/DiemThiXetTuyenDAO.java
@@ -0,0 +1,5 @@
+package com.tuyensinh.dao;
+
+public class DiemThiXetTuyenDAO {
+    
+}
--- src/main/java/com/tuyensinh/dao/NhomQuyenDAO.java
+++ src/main/java/com/tuyensinh/dao/NhomQuyenDAO.java
@@ -1,5 +1,5 @@
 package com.tuyensinh.dao;
 
-public class DiemThiSinhDAO {
+public class NhomQuyenDAO {
     
 }
