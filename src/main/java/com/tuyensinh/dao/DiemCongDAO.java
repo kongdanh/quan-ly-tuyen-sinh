d9754bcc904f3b46331e55babbb470895dd2a1dc
Auresol69 <quocbao962005@gmail.com>
2026-03-31 20:40:13 +0700

    Thêm Hibernate utils, abstract class Generic DAO, và mn có thể xem mẫu bên DiemCongDAO

--- src/main/java/com/tuyensinh/dao/DiemCongDAO.java
+++ src/main/java/com/tuyensinh/dao/DiemCongDAO.java
@@ -1,5 +1,44 @@
 package com.tuyensinh.dao;
 
-public class DiemCongDAO {
-    
+import com.tuyensinh.model.DiemCong;
+import com.tuyensinh.util.HibernateUtil;
+
+public class DiemCongDAO extends GenericDAO<DiemCong>{
+
+    public DiemCongDAO(Class<DiemCong> entityClass) {
+        super(entityClass);
+    }
+
+    public DiemCongDAO() {
+        super(DiemCong.class);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Start Async "+ DiemCong.class);
+        GenericDAO<DiemCong> dao = new DiemCongDAO();
+        dao.findAll().thenAccept(diemCong -> {
+            if (diemCong !=null) {
+                System.out.println("Thành công! Data: " + diemCong);
+                for (DiemCong dc : diemCong) {
+                    System.out.println(dc.getId());
+                }
+            }
+            else
+                System.out.println("Không tìm thấy data trong "+ DiemCong.class+ ".");
+        }).exceptionally(throwable -> {
+            System.err.println("Lỏ rồi " + throwable.getMessage());
+            return null;
+        }).whenComplete((result, throwable) -> {
+            if (throwable != null) {
+                System.err.println("Lỏ rồi: " + throwable.getMessage());
+            } else {
+                System.out.println("Thành công: " + result);
+            }
+            System.out.println("Đang shutdown pool...");
+            dao.shutdown();
+            HibernateUtil.shutdown();
+        });
+
+        System.out.println("Hàm main continue chạy mà không đợi DB");
+    }
 }
--- src/main/java/com/tuyensinh/dao/GenericDAO.java
+++ src/main/java/com/tuyensinh/dao/GenericDAO.java
@@ -1,5 +1,58 @@
 package com.tuyensinh.dao;
 
-public class GenericDAO {
-    
+import com.tuyensinh.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public abstract class GenericDAO<T> {
+    public final Class<T> entityClass;
+    private static final ExecutorService dbThreadPool = Executors.newFixedThreadPool(10);
+
+    public GenericDAO(Class<T> entityClass){
+        this.entityClass = entityClass;
+    }
+
+    public void saveOrUpdate(T entity){
+        try(Session session = HibernateUtil.createSession(Session.class)){
+            Transaction tx = session.beginTransaction();
+            entity = session.merge(entity);
+            tx.commit();
+        }
+    }
+
+    public CompletableFuture<T> findById(Object id){
+        return CompletableFuture.supplyAsync(() -> {
+            try(Session session = HibernateUtil.createSession(Session.class)){
+                return session.get(entityClass, (java.io.Serializable) id);
+            }
+        }, dbThreadPool);
+    }
+
+    public CompletableFuture<List<T>> findAll(){
+        return CompletableFuture.supplyAsync(()->{
+            try(Session session = HibernateUtil.createSession(Session.class)){
+                return session.createQuery("FROM " + entityClass.getName(), entityClass)
+                        .list();
+            }
+        }, dbThreadPool);
+    }
+
+    public void delete(T entity){
+        try(Session session = HibernateUtil.createSession(Session.class)){
+            Transaction tx = session.beginTransaction();
+            T managedEntity = session.merge(entity);
+            if (managedEntity != null)
+                session.remove(managedEntity);
+            tx.commit();
+        }
+    }
+
+    public void shutdown(){
+        dbThreadPool.shutdown();
+    }
 }
